c856a46c82a4eb46cf5f78272be54120b4d4b607
ihavenoideawhattoputin <guizaouiadam@gmail.com>
2024-03-20 10:41:45 +0100

    premier jet

--- evaluateur.h
+++ evaluateur.h
@@ -2,9 +2,11 @@
 #define EVALUATUEUR_H
 #include "jeton.h"
 
+float Evaluateur(Node* A, float x);
 
+float* Eval_Arbre_Range_x(struct Node* Arbre, float x_min, float x_max, int n);
 
-
+float* Eval_Arbre_Range_y(struct Node* Arbre, float x_min, float x_max, int n);
 
 
 
